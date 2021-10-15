63db883e6079784dd6bd662925a1eccff4937f03
ArandomPersonHere <72626909+ArandomPersonHere@users.noreply.github.com>
2021-10-15 11:05:06 -0600

    idk

--- template/sketch.js
+++ template/sketch.js
@@ -1,35 +1,27 @@
 
 // 
-
-let rectHeights = [];
-let numberOfRects = 400;
+let gridBlock;
+let gridSize = 10;
+// let cols = 5;
+// let rows = 5;
 
 function setup() {
   
   createCanvas(windowWidth, windowHeight);
-  gennerateTerrain();
+  gridBlock = createEmpty2darr(gridSize, gridSize);
+  
 }
 
 function draw() {
   background("purple");
-}
 
-function displayTerrain(){
-  let theWidth = width/ rectHeights.length;
-  for (let i =0; i<rectHeights.length; i++){
-    let theHeight = rectHeights[i];
-    fill("black");
-    rect(theWidth*i, height, 10, - theHeight);
 
-  }
 
 }
 
-function gennerateTerrain(){
-  let time = 0;
-  for (let i = 0; i<100; i++){
-    let theHeight = noise(time) * height;
-    rectHeights.push(theHeight);
-    time += 0.002;
-  }
+function createEmpty2darr() {
+
+  for 
+
+  rect(x, y, gridBlock[0], gridBlock[1]);
 }
