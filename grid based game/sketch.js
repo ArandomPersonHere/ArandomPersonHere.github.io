f7ecf4042dd3b6292b99c18230fa21c51ed1a844
ArandomPersonHere <72626909+ArandomPersonHere@users.noreply.github.com>
2021-10-19 11:57:21 -0600

    made an index,  and website link

--- grid based game/sketch.js
+++ grid based game/sketch.js
@@ -1,35 +1,13 @@
 
 // 
 
-let rectHeights = [];
-let numberOfRects = 400;
 
 function setup() {
   
   createCanvas(windowWidth, windowHeight);
-  gennerateTerrain();
+  
 }
 
 function draw() {
   background("purple");
-}
-
-function displayTerrain(){
-  let theWidth = width/ rectHeights.length;
-  for (let i =0; i<rectHeights.length; i++){
-    let theHeight = rectHeights[i];
-    fill("black");
-    rect(theWidth*i, height, 10, - theHeight);
-
-  }
-
-}
-
-function gennerateTerrain(){
-  let time = 0;
-  for (let i = 0; i<100; i++){
-    let theHeight = noise(time) * height;
-    rectHeights.push(theHeight);
-    time += 0.002;
-  }
 }
