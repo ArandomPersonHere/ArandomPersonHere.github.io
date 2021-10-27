48e2d0be51f04aac7b659b543a82b342e6d6dd73
ArandomPersonHere <72626909+ArandomPersonHere@users.noreply.github.com>
2021-10-27 12:02:29 -0600

    OOP walker demo,
    made an object, and a dot maker

--- walker demo/sketch.js
+++ walker demo/sketch.js
@@ -1,11 +1,57 @@
-// First Demo
+// OOP walker demo
+let kayaan, nick;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  kayaan = new Walker(width/3, height/3,"blue");
+  nick = new Walker(width/3*2,  height/3, "purple");
+  hannah = new Walker(width/2,  height/2, "red");
+ 
+
 }
 
 function draw() {
-  background(220);
+  kayaan.move();
+  nick.move();
+  hannah.move();
+  hannah.display();
+  nick.display();
+  kayaan.display();
 
-  rect(mouseX, mouseY, 100, 50);
+}
+
+
+class Walker {
+  constructor (x, y, theColor){
+    this.x = x;
+    this.y = y;
+    this.color = theColor;
+    this.speed = random(25);
+    this.size = this.speed;
+  }
+
+  display(){
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y, this.size);
+  }
+
+  move(){
+    let choice = random(100);
+    if (choice<25 && this.y >=0){
+      this.y -=this.speed;
+    }
+    else if (choice<50 && this.y <=height){
+      this.y +=this.speed;
+    }
+    else if (choice<75&& this.y >=0){
+      this.x -=this.speed;
+    }
+    else if (choice<100&& this.y <=width){
+      this.x +=this.speed;
+    }
+
+
+  }
 }
