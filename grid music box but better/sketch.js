95c2992cfb7d946cdc9a4cefe7817c3632e166cf
ArandomPersonHere <72626909+ArandomPersonHere@users.noreply.github.com>
2021-11-03 12:12:44 -0600

    got rid of ifs and else ifs

--- grid music box but better/sketch.js
+++ grid music box but better/sketch.js
@@ -1,16 +1,20 @@
 //grid project 
 // what i want it to do 
-// 1 - drag and play different audio filesinto different parts of the grid
+// 1 - play different audio files in different parts of the grid
 // 2 - simultainiously play audio files in a collum
 // 3 - have an auto play feature that visually dispays what collum it's playing
-// 4 - 
+
+
+
 
 
 // 
 let gridSize = 20;
 let grid;
 //let mysound =[];
-let whiteOrb, yellowOrb,greenOrb, blueOrb, greyOrb, swirlOrb, reallyWhiteOrb; 
+
+
+let whiteOrb, yellowOrb, greenOrb,  blueOrb,  greyOrb,  swirlOrb,  reallyWhiteOrb;
 let monoSynth;
 let gridState = [];
 
@@ -36,9 +40,6 @@ function preload(){
   greyOrb = loadImage("assets/greYOrb.png");
   swirlOrb = loadImage("assets/Empty Orb.png");
   reallyWhiteOrb = loadImage("assets/LightOrb.png");
-
-
-
 }
 
 function setup() {
@@ -49,7 +50,7 @@ function setup() {
 
   monoSynth = new p5.MonoSynth();
   // a simple C scale with C4 = 0
-  note = ["C4", "D4", "E4", "A4", "B4","Fb4", "G4", "C5", "B3"];
+  note = [ "A4","B4", "C4", "D4", "E4","F4", "G4","A5", "B5", "C5"];
   velocity = random();
 }
 
@@ -63,9 +64,6 @@ function draw() {
 function keyPressed(){
   if (key === "e"){
     grid = createEmpty2DArray(gridSize, gridSize);
-  }
-  if (key === "b"){
-    grid = createEmpty2DArray(gridSize, gridSize, 1);
   }
   if (key === "r"){
     grid = createRandom2DArray(gridSize, gridSize);
@@ -82,11 +80,10 @@ function linePlayer(){
   // without changing the tiles
 
   for (let i = 0; i <gridSize; i++){
-    for (let b = 0; b <gridSize;b++){
-
-      playSynth(grid[i][b]);
-      console.log(grid[i][b]);
-    }
+    
+    playSynth(grid[i][0]);
+    console.log(grid[i][0]);
+  
   }
 
 
@@ -112,77 +109,42 @@ function mousePressed() {
   let cellX = Math.floor(mouseX/ cellWidth);
   let cellY = Math.floor(mouseY/ cellHeight);
 
-  
-
-  if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
-    // mysound[3].play();
-    playSynth(0);
+  //master if/else statement to avoid pain
+  if  (grid[cellY][cellX] === 6) {
+    playSynth(grid[cellY][cellX]);
+    grid[cellY][cellX]  = 0;
   }
-  else if (grid[cellY][cellX] === 1) {
-    grid[cellY][cellX] = 2;
-    playSynth(1);
+  else{
+    playSynth(grid[cellY][cellX]);
+    grid[cellY][cellX] ++;
   }
-  else if (grid[cellY][cellX] === 2) {
-    grid[cellY][cellX] = 3;
-    playSynth(2);
-  }
-  else if (grid[cellY][cellX] === 3) {
-    grid[cellY][cellX] = 4;
-    playSynth(3);
-  }
-  else if (grid[cellY][cellX] === 4) {
-    grid[cellY][cellX] = 5;
-    playSynth(4);
-  }
-  else if (grid[cellY][cellX] === 5) {
-    grid[cellY][cellX] = 6;
-    playSynth(5);
-  }
-  else if (grid[cellY][cellX] === 6) {
-    grid[cellY][cellX] = 0;
-    playSynth(8);
-  }
-  
+  //saved 50 lines
 }
 
 
 
 function displayGrid(){
+  let myOrbs = [
+    whiteOrb,
+    yellowOrb,
+    greenOrb, 
+    blueOrb, 
+    greyOrb, 
+    swirlOrb, 
+    reallyWhiteOrb
+  ]; 
   
   let cellWidth = width/gridSize;
   let cellHeight = height/gridSize;
+
   for (let y = 0; y<gridSize; y++){
     for (let x = 0; x<gridSize; x++){
-
       //make grid icons
-
-      if (grid[y][x] === 0){
-        image(whiteOrb,x *cellWidth, y *cellHeight, cellWidth, cellHeight); 
-      }
-      else if (grid[y][x] === 1){
-        image(yellowOrb,x *cellWidth, y *cellHeight, cellWidth, cellHeight); 
-      }
-      else if (grid[y][x] === 2){
-        image(greenOrb,x *cellWidth, y *cellHeight, cellWidth, cellHeight); 
-      }
-      else if (grid[y][x] === 3){
-        image(blueOrb,x *cellWidth, y *cellHeight, cellWidth, cellHeight); 
-      }
-      else if (grid[y][x] === 4){
-        image(greyOrb,x *cellWidth, y *cellHeight, cellWidth, cellHeight); 
-      }
-      else if (grid[y][x] === 5){
-        image(swirlOrb,x *cellWidth, y *cellHeight, cellWidth, cellHeight); 
-      }
-      else if (grid[y][x] === 6){
-        image(reallyWhiteOrb,x *cellWidth, y *cellHeight, cellWidth, cellHeight); 
-      }
-     
+      //master statement to avoid pain
+      image(myOrbs[grid[y][x]],x *cellWidth, y *cellHeight, cellWidth, cellHeight);
+      // saved 25 lines
     }
-
   }
- 
 }
 
 function createRandom2DArray(rows,cols, numToFill = 0){
@@ -190,27 +152,34 @@ function createRandom2DArray(rows,cols, numToFill = 0){
   for (let y = 0; y<rows; y++){
     grid.push([]);
     for (let x = 0; x<cols; x++) {
-      if (random(100) <10){
-        grid[y].push(0);
-      }
-      else if (random(100) <20){
-        grid[y].push(1);
-      }
-      else if (random(100) <30){
-        grid[y].push(2);
-      }
-      else if (random(100) <40){
-        grid[y].push(3);
-      }
-      else if (random(100) <50){
-        grid[y].push(4);
-      }
-      else if (random(100) <60){
-        grid[y].push(5);
-      }
-      else{
-        grid[y].push(6);
-      }
+
+      // one line to save you from if/ifelse pain
+      let num = random(0, 7);
+
+      grid[y].push(num);
+      // // saves 20 lines
+
+      // if (random(100) <10){
+      //   grid[y].push(0);
+      // }
+      // else if (random(100) <20){
+      //   grid[y].push(1);
+      // }
+      // else if (random(100) <30){
+      //   grid[y].push(2);
+      // }
+      // else if (random(100) <40){
+      //   grid[y].push(3);
+      // }
+      // else if (random(100) <50){
+      //   grid[y].push(4);
+      // }
+      // else if (random(100) < 60){
+      //   grid[y].push(5);
+      // }
+      // else{
+      //   grid[y].push(6);
+      // }
     }
     
   }
