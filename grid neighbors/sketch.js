6c232bb796b666aafbb730b60b6234128c3ad86b
ArandomPersonHere <72626909+ArandomPersonHere@users.noreply.github.com>
2021-11-03 11:13:58 -0600

    copy paste

--- grid neighbors/sketch.js
+++ grid neighbors/sketch.js
@@ -1,82 +1,100 @@
-//grid demo 2D Array
-// 
-let gridSize = 50;
+let gridSize = 30;
 let grid;
-let mysound = []
+let clickSound;
+
+function preload() {
+  clickSound = loadSound("assets/click.wav");
+}
 
 function setup() {
-  
   createCanvas(windowWidth, windowHeight);
   grid = createRandom2DArray(gridSize, gridSize);
 }
 
 function draw() {
-  background("green");
+  background(220);
   displayGrid();
- 
+}
 
+function keyPressed() {
+  if (key === "e") {
+    grid = createEmpty2DArray(gridSize, gridSize);
+  }
+  if (key === "b") {
+    grid = createEmpty2DArray(gridSize, gridSize, 1);
+  }
+  if (key === "r") {
+    grid = createRandom2DArray(gridSize, gridSize);
+  }
 }
 
 function mousePressed() {
+  clickSound.play();
+
   let cellWidth = width/gridSize;
   let cellHeight = height/gridSize;
 
   let cellX = Math.floor(mouseX/cellWidth);
   let cellY = Math.floor(mouseY/cellHeight);
 
-if (grid[cellY][cellX] === 0){
-  grid [cellY][cellX] = 1;
-}
-else if (grid[cellY][cellX] === 1){
-    grid [cellY][cellX] = 0;
-
-}
-
+  swap(cellX, cellY);
+  swap(cellX+1, cellY);
+  swap(cellX-1, cellY);
+  swap(cellX, cellY+1);
+  swap(cellX, cellY-1);
 }
 
-function displayGrid(){
+function swap(x, y) {
+  if (x >= 0 && x < gridSize && y >=0 && y < gridSize) {
+    if (grid[y][x] === 1) {
+      grid[y][x] = 0;
+    }
+    else if (grid[y][x] === 0) {
+      grid[y][x] = 1;
+    }
+  }
+}
+
+function displayGrid() {
   let cellWidth = width/gridSize;
   let cellHeight = height/gridSize;
-  for (let y = 0; y<gridSize; y++){
-    for (let x = 0; x<gridSize; x++){
-      if (grid[y][x] === 0){
-        fill("blue");
-      }
-      else if (grid[y][x] === 1){
-        fill("red");
-      }
 
-      rect (x *cellWidth, y *cellHeight, cellWidth, cellHeight);
+  for (let y=0; y<gridSize; y++) {
+    for (let x=0; x<gridSize; x++) {
+      if (grid[y][x] === 0) {
+        fill("white");
+      }
+      else if (grid[y][x] === 1) {
+        fill("black");
+      }
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
-
   }
-
 }
 
-
-function createRandom2DArray(rows,cols){
+function createEmpty2DArray(rows, cols, numToFill = 0) {
   let grid = [];
-  for (let y = 0; y<rows; y++){
+  for (let y=0; y<rows; y++) {
     grid.push([]);
-    for (let x = 0; x<cols; x++) {
-      if (random(100) <50){
-        grid[y].push(0);
-      }
-      else{
-        grid[y].push(1);
-      }
+    for (let x=0; x<cols; x++) {
+      grid[y].push(numToFill);
     }
-    
   }
   return grid;
 }
-// function createEmpty2DArray(rows,cols){
-//   let grid = [];
-//   for (let y = 0; y<rows; y++){
-//     grid.push([]);
-//     for (let x = 0; x<cols; x++);
-//     grid[y].push(0);
-//   }
-//   return grid;
-// }
 
+function createRandom2DArray(rows, cols) {
+  let grid = [];
+  for (let y=0; y<rows; y++) {
+    grid.push([]);
+    for (let x=0; x<cols; x++) {
+      if (random(100) < 50) {
+        grid[y].push(0);
+      }
+      else {
+        grid[y].push(1);
+      }
+    }
+  }
+  return grid;
+}
